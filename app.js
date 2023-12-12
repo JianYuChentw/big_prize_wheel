265b33246c2722c4954759bec1a98faa270b6986
JianYuChentw <coke818572@gmail.com>
2023-12-12 11:09:29 +0800

    更新app.js $ routers 加入後台前端

--- app.js
+++ app.js
@@ -32,10 +32,15 @@ app.use((req, res, next) => {
 app.use(express.json({ limit: '1mb' }));
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist_admin')));
 
 app.use('/', routes);
 app.get('/player/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.get('/admin/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist_admin', 'index.html'));
 });
 
 //jian修改
